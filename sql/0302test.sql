7897df958a01f1fe2d074a18cf6d51050d7eeaa3
鄭伊慈 <kk696.test@gmail.com>
2024-03-02 11:09:04 +0800

    Update 0302test.sql

--- sql/0302test.sql
+++ sql/0302test.sql
@@ -1,1 +1,13 @@
 select now() as 現在時間
+
+CREATE DATABASE test;
+
+--DROP DATABASE test;
+
+
+CREATE TABLE IF NOT EXISTS student(
+	student_id SERIAL PRIMARY KEY,
+	name VARCHAR(20),
+	major VARCHAR(20)
+);
+
