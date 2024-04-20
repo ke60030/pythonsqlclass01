1b02b004c334d0e8f37a41049cbf2b64377ad423
鄭伊慈 <161096778+ke60030@users.noreply.github.com>
2024-04-20 03:58:07 +0000

    20240420

---
+++ lesson15/lesson15_2.py
@@ -0,0 +1,15 @@
+import streamlit as st
+
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+st.title("計數器的範例")
+
+increment = st.button("增加計數器的值",key="mybutton")
+if increment:
+    st.session_state.count += 1
+
+st.write("計數器:",st.session_state.count)
+
+
+
